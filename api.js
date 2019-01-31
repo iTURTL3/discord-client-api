5d6a565aecffe1eccd162a91c1319f3c3743114f
jakemadness <j4kem4dness@live.com>
2019-01-31 01:43:16 +0000

    Update api.js
    All headers are now attached in the apiRequest function

--- api.js
+++ api.js
@@ -1,10 +1,7 @@
-/*
-   @https://github.com/jakemadness/discord-client-api
-*/
-window.discordClient = function(token) {
+window.discordClientApi = function(token) {
    var self         = this;
-   self.version     = 6;
-   self.endPoint    = 'https://discordapp.com/api/v' + self.version;
+   self.apiVersion  = 6;
+   self.apiURL      = 'https://discordapp.com/api/v' + self.apiVersion + '/';
    self.httpRequest = function(method, url, headers, post, callback) {
       var request = new XMLHttpRequest();
       request.onreadystatechange = function() {
@@ -16,126 +13,62 @@ window.discordClient = function(token) {
       }
       request.send(post);
    };
-   self.acceptInvite = function(inviteCode, callback) {
-      self.httpRequest('POST', self.endPoint + '/invite/' + inviteCode, [
+   self.apiRequest = function(method, path, post, callback) {
+      self.httpRequest(method, self.apiURL + path, [
          'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/x-www-form-urlencoded; charset=UTF-8',
-         'authorization',    token
-      ], null, callback);
+         'authorization',    token,
+         'content-type',     post ? 'application/json' : 'application/x-www-form-urlencoded'
+      ], post, callback);
+   };
+   self.joinServer = function(inviteCode, callback) {
+      self.apiRequest('POST', 'invite/' + inviteCode, null, callback);
    };
    self.leaveServer = function(serverId, callback) {
-      self.httpRequest('DELETE', self.endPoint + '/users/@me/guilds/' + serverId, [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/x-www-form-urlencoded; charset=UTF-8',
-         'authorization',    token
-      ], null, callback);
+      self.apiRequest('DELETE', 'users/@me/guilds/' + serverId, null, callback);
    };
-   self.isTyping = function(channelId, callback) {
-      self.httpRequest('POST', self.endPoint + '/channels/' + channelId + '/typing', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/x-www-form-urlencoded; charset=UTF-8',
-         'authorization',    token
-      ], null, callback);
+   self.sendMessage = function(channelId, message, callback) {
+      self.apiRequest('POST', 'channels/' + channelId + '/messages', JSON.stringify({'content': message, 'nonce': Date.now(), 'tts': false}), callback);
+   };
+   self.deleteMessage = function(channelId, messageId, callback) {
+      self.apiRequest('DELETE', 'channels/' + channelId + '/messages/' + messageId, null, callback);
    };
    self.addReaction = function(channelId, messageId, reaction, callback) {
-      self.httpRequest('PUT', self.endPoint + '/channels/' + channelId + '/messages/' + messageId + '/reactions/' + reaction + '/@me', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/x-www-form-urlencoded; charset=UTF-8',
-         'authorization',    token
-      ], null, callback);
+      self.apiRequest('PUT', 'channels/' + channelId + '/messages/' + messageId + '/reactions/' + reaction + '/@me', null, callback);
    };
    self.removeReaction = function(channelId, messageId, reaction, callback) {
-      self.httpRequest('DELETE', self.endPoint + '/channels/' + channelId + '/messages/' + messageId + '/reactions/' + reaction + '/@me', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/x-www-form-urlencoded; charset=UTF-8',
-         'authorization',    token
-      ], null, callback);
+      self.apiRequest('DELETE', 'channels/' + channelId + '/messages/' + messageId + '/reactions/' + reaction + '/@me', null, callback);
    };
-   self.sendMessage = function(channelId, message, nonce, callback) {
-      self.httpRequest('POST', self.endPoint + '/channels/' + channelId + '/messages', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], JSON.stringify({
-         'content': message,
-         'nonce':   nonce,
-         'tts':     false
-      }), callback);
+   self.isTyping = function(channelId, callback) {
+      self.apiRequest('POST', 'channels/' + channelId + '/typing', null, callback);
    };
    self.getMessages = function(channelId, amount, callback) {
-      self.httpRequest('GET', self.endPoint + '/channels/' + channelId + '/messages?limit=' + amount, [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], null, callback);
+      self.apiRequest('GET', 'channels/' + channelId + '/messages?limit=' + amount, null, callback);
    };
-   self.updateNickname = function(serverId, nickname, callback) {
-      self.httpRequest('PATCH', self.endPoint + '/guilds/' + serverId + '/members/@me/nick', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], JSON.stringify({
-         'nick': nickname
-      }), callback);
+   self.changeNickname = function(serverId, nickname, callback) {
+      self.apiRequest('PATCH', 'guilds/' + serverId + '/members/@me/nick', JSON.stringify({'nick': nickname}), callback);
    };
    self.updateStatus = function(status, callback) {
-      self.httpRequest('PATCH', self.endPoint + '/users/@me/settings', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], JSON.stringify({
-         'status': status
-      }), callback);
+      self.apiRequest('PATCH', 'users/@me/settings', JSON.stringify({'status': status}), callback);
    };
    self.updateUserSettings = function(username, email, password, newPassword, avatarBase64, callback) {
-      self.httpRequest('PATCH', self.endPoint + '/users/@me', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], JSON.stringify({
-         'username':      username,
-         'email':         email,
-         'password':      password,
-         'new_password':  newPassword,
-         'avatar':        avatarBase64,
-         'discriminator': null
-      }), callback);
+      self.apiRequest('PATCH', 'users/@me', JSON.stringify({'username': username, 'email': email, 'password': password, 'new_password': newPassword, 'avatar': avatarBase64, 'discriminator': null}), callback);
    };
    self.createServer = function(name, region, icon, callback) {
-      self.httpRequest('POST', self.endPoint + '/guilds', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], JSON.stringify({
-         'name':   name,
-         'region': region,
-         'icon':   icon
-      }), callback);
+      self.apiRequest('POST', 'guilds', JSON.stringify({'name': name, 'region': region, 'icon': icon}), callback);
    };
    self.deleteServer = function(serverId, callback) {
-      self.httpRequest('POST', self.endPoint + '/guilds/' + serverId + '/delete', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], JSON.stringify({}), callback);
+      self.apiRequest('POST', 'guilds/' + serverId + '/delete', JSON.stringify({}), callback);
    };
    self.createChannel = function(serverId, name, parentId, type) {
-      self.httpRequest('POST', self.endPoint + '/guilds/' + serverId + '/channels', [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], JSON.stringify({
-         'name':                  name,
-         'parent_id':             parentId,
-         'type':                  type,
-         'permission_overwrites': []
-      }), callback);
+      self.apiRequest('POST', 'guilds/' + serverId + '/channels', JSON.stringify({'name': name, 'parent_id': parentId, 'type': type, 'permission_overwrites': []}), callback);
    };
    self.deleteChannel = function(channelId, callback) {
-      self.httpRequest('DELETE', self.endPoint + '/channels/' + channelId, [
-         'x-requested-with', 'XMLHttpRequest',
-         'content-type',     'application/json; charset=UTF-8',
-         'authorization',    token
-      ], null, callback);
+      self.apiRequest('DELETE', 'channels/' + channelId, null, callback);
+   };
+   self.createRole = function(serverId, callback) {
+      self.apiRequest('POST', 'guilds/' + serverId + '/roles', null, callback);
+   };
+   self.deleteRole = function(serverId, roleId, callback) {
+      self.apiRequest('DELETE', 'guilds/' + serverId + '/roles/' + roleId, null, callback);
    };
 };
