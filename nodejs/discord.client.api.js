cc523df0fa707b472e36cba5535cc109f2cea9f4
jakemadness <j4kem4dness@live.com>
2019-03-02 11:43:35 +0000

    Update discord.client.api.js

--- nodejs/discord.client.api.js
+++ nodejs/discord.client.api.js
@@ -114,4 +114,8 @@ module.exports = function(token) {
       self.apiRequest('POST', 'hypesquad/online', JSON.stringify({'house_id': houseNumber}), callback);
    };
 
+   self.createInvite = function(channelId, callback) {
+      self.apiRequest('POST', 'channels/' + channelId + '/invites', JSON.stringify({'max_age': 0, 'max_uses': 0, 'temporary': false}), callback);
+   };
+
 };
